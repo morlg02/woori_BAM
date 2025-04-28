d364142de47a81ad5fcff50869e6a9d74fd71909
dkrdj896 <dkrdj896@gmail.com>
2025-04-28 17:15:09 +0900

    article list ==> size(0, get() 학습)

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
  			System.out.printf("명령어) ");
  			String cmd = sc.nextLine().trim(); 
  
- 
  			if (cmd.equals("exit")) {
  				break;
  			} 
@@ -21,57 +20,48 @@ import java.util.ArrayList;
  				System.out.println("명령어를 입력해 주세요");
  				continue;
  			}
- 
+ 			// List 메서드중 size() 이용 (Data 유무를 객체의 객수(크기)로 변환)
  			if (cmd.equals("article list")) {
- 				System.out.println("게시글이 없습니다");
- 			} else if(cmd.equals("article write")){
+ 				if (articles.size() == 0 ) {
+ 					System.out.println("존재하는 게시글이 없습니다");
+ 					continue;
+ 					
+ 				}
  
+ 				// 배열 사용해서 get() 사용 ==> 객체를 리턴 받음
+ 				System.out.printf("번호    |     제목\n");
+ 				for (int i = articles.size() -1 ; i >= 0; i--) {
+ 					// article은 articles.get() 을 통해 받은 객체를 재사용하기 위해 저장 용도로 사용됨 
+ 					Article article = articles.get(i);
+ 					System.out.printf("%d      |     %s\n", article.id, article.title);
+ 
+ 				}
+ 				
+ 			} else if(cmd.equals("article write")){
  
  				System.out.printf("제목 : ");
  				String title = sc.nextLine().trim(); 
  				System.out.printf("내용 : ");
  				String body = sc.nextLine().trim(); 
- 				
- //				Article article = new Article(); // 최적화 사용 안함,  article 은 지역변수
- 				Article article = new Article(id,title,body); // 인자를 통해 생성자 호출
- 				// 최적화 작업 : 생성자를 이용해서 초기화
- //				article.id = id;                 // 따라서 저장이 된거는 아님
- //				article.title = title;
- //				article.body = body;
- 				
- 				// 진짜 저장은 아래 문장을 통해서 진행됩니다
- 				articles.add(article);  // List구조인 ArrayList 객체인 articles 저장
- 				
- 				// 최적화 2
- //				articles.add(new Article(id,title,body));
- 				
+ 				Article article = new Article(id,title,body); 
+ 				articles.add(article); 
+ 
  				System.out.println(id +"번글이 생성되었습니다");
  				id++;
  
- 			// 추후 아래 else if문은 삭제 예정.  명령어 test로 articles 저장된 내용 확인
- 			// articles은 ArrayList 자료구조로 인덱스를 가지고 객체수는 제한 없음	
- 			} else if (cmd.equals("test")) {
- 				for(int i =0 ; i < articles.size(); i++) {
- 					System.out.println(articles.get(i).id);
- 					System.out.println(articles.get(i).title);
- 					System.out.println(articles.get(i).body);
- 				}  // 여기까지 삭제 할 거에요
- 				
- 				
  			}else {
  				System.out.println("존재하지 않는 명령어 입니다");
  			}
  		}
- 
  		sc.close();
  		System.out.println("== 프로그램 종료 ==");
  	}
- 
  }
  class Article {
  	int id;
  	String title;
  	String body;
+ 
  	public Article(int id, String title, String body) {
  		this.id = id;
  		this.title = title;
