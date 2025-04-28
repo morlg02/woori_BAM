1810ff74a059d902586c5dd732b054c41064deff
dkrdj896 <dkrdj896@gmail.com>
2025-04-28 17:09:31 +0900

    article write 구현중

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,41 @@ import java.util.Scanner;
      
  	public static void main(String[] args) {
  		System.out.println("== 프로그램 시작 ==");
- 		Scanner sc = new Scanner(System.in); // 최적화 => 자원 1번 사용 개선
-        
- 		while (true) {
- 			System.out.printf("cmd) ");
- 			String cmd = sc.nextLine(); // cmd 변수 ==> 재사용 하기 위해
- 			System.out.println("명령어) " + cmd);
- 			if (cmd.equals("exit")) {
- //	        if (cmd == "exit") {
- 				break;
- 			}
+ 		Scanner sc = new Scanner(System.in); 
  
+ 		int id = 1;
+ 		
+ 		while (true) {
+ 			System.out.printf("명령어) ");
+ 			String cmd = sc.nextLine().trim(); 
+ 			
+ 			
+ 			if (cmd.equals("exit")) {
+ 				break;
+ 			} 
+ 			if (cmd.length() == 0) {
+ 				System.out.println("명령어를 입력해 주세요");
+ 				continue;
+ 			}
+ 			
+ 			if (cmd.equals("article list")) {
+ 				System.out.println("게시글이 없습니다");
+ 			} else if(cmd.equals("article write")){
+ 				
+ 				
+ 				System.out.printf("제목 : ");
+ 				String title = sc.nextLine().trim(); 
+ 				System.out.printf("내용 : ");
+ 				String body = sc.nextLine().trim(); 
+ 				System.out.println(id +"번글이 생성되었습니다");
+ 				id++;
+ 				
+ 			}else {
+ 				System.out.println("존재하지 않는 명령어 입니다");
+ 			}
  		}
+ 		
  		sc.close();
  		System.out.println("== 프로그램 종료 ==");
- 
  	}
- 
  }
