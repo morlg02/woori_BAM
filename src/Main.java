8c10fbcdc348970b0558625977b5d5b69a054e3f
dkrdj896 <dkrdj896@gmail.com>
2025-04-28 17:17:04 +0900

    article detail 구현중

--- src/Main.java
+++ src/Main.java
@@ -21,34 +21,37 @@ import java.util.ArrayList;
  				continue;
  			}
  			// List 메서드중 size() 이용 (Data 유무를 객체의 객수(크기)로 변환)
- 			if (cmd.equals("article list")) {
- 				if (articles.size() == 0 ) {
- 					System.out.println("존재하는 게시글이 없습니다");
- 					continue;
- 					
- 				}
- 
- 				// 배열 사용해서 get() 사용 ==> 객체를 리턴 받음
- 				System.out.printf("번호    |     제목\n");
- 				for (int i = articles.size() -1 ; i >= 0; i--) {
- 					// article은 articles.get() 을 통해 받은 객체를 재사용하기 위해 저장 용도로 사용됨 
- 					Article article = articles.get(i);
- 					System.out.printf("%d      |     %s\n", article.id, article.title);
- 
- 				}
- 				
- 			} else if(cmd.equals("article write")){
- 
+ 			if (cmd.equals("article write")) {
+ 		
  				System.out.printf("제목 : ");
  				String title = sc.nextLine().trim(); 
  				System.out.printf("내용 : ");
  				String body = sc.nextLine().trim(); 
+ 
  				Article article = new Article(id,title,body); 
  				articles.add(article); 
  
  				System.out.println(id +"번글이 생성되었습니다");
  				id++;
  
+ 			} else if(cmd.equals("article list")){
+ 
+ 				if (articles.size() == 0 ) {
+ 					System.out.println("존재하는 게시글이 없습니다");
+ 					continue;
+ 					
+ 				}
+ 				
+ 				System.out.printf("번호    |     제목\n");
+ 				for (int i = articles.size() -1 ; i >= 0; i--) {
+ 					Article article = articles.get(i);
+ 					System.out.printf("%d      |     %s\n", article.id, article.title);
+ 				}
+ 			} else if (cmd.startsWith("article detail ")) { // article detail 로 시작하니?
+ 				String[] cmdBits = cmd.split(" ");
+ 				System.out.println(cmdBits[0]);
+ 				System.out.println(cmdBits[1]);
+ 				System.out.println(cmdBits[2]);
  			}else {
  				System.out.println("존재하지 않는 명령어 입니다");
  			}
